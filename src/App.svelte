dccb3f3a55c8fde906d367c1b8794723a7d96724
jazzhammer <tio.a@tacteonltd.com>
2023-09-24 18:01:43 -0700

    chore(src): primordial

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+  import TailwindCss from './lib/TailwindCSS.svelte';
+</script>
+<TailwindCss/>
+
+<main class="w-full p-0 m-0 h-fit">
+
+</main>
+
+<style>
+</style>
